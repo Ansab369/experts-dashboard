5f6558c2c4fa5bbbf9cef5e7c479541dfc6f02a5
Ansab <Ansab.dev@gmail.com>
2022-12-26 10:11:52 +0530

    fix bug2

--- src/screens/expertsPublic/containers/socialmedia/import.js
+++ src/screens/expertsPublic/containers/socialmedia/import.js
@@ -1,7 +1,7 @@
 import Facebook from '../../../../assets/expertsAssets/Facebook.png';
 import Instagram from '../../../../assets/expertsAssets/Instagram.png';
 import Twitter from '../../../../assets/expertsAssets/Twitter.png';
-import Youtube from '../../../../assets/expertsAssets/youtube.png';
+// import Youtube from '../../../../assets/expertsAssets/youtube.png';
 import LinkedIn from '../../../../assets/expertsAssets/linkedIn.png';
 
 
@@ -10,6 +10,6 @@ export {
     Facebook,
     Instagram,
     Twitter,
-    Youtube,
+    // Youtube,
     LinkedIn,
 }
